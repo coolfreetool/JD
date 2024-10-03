8e1f798c49aa0c9301116b50049cce74c8cce2fb
Josef Hak <pepa.hak@gmail.com>
2024-10-03 09:32:32 +0200

    Add MIP test

--- HighsJD/README.md
+++ HighsJD/README.md
@@ -1,5 +1,6 @@
 # HiGHS JD connector
 
 ~~~bash
-dotnet test --framework net5.0
+dotnet test --framework net5.0 # run all tests
+dotnet test --framework net5.0 --filter PassMip1Test # run specific test
 ~~~
--- HighsJD/tests/ExampleTest.cs
+++ HighsJD/tests/ExampleTest.cs
@@ -13,7 +13,7 @@ namespace Tests
     internal class ExampleTest
     {
         [Test]
-        public void Example1Test()
+        public void PassLp1Test()
         {      
             double[] cc = {1, -2};
             double[] cl = {0, 0};
@@ -39,6 +39,50 @@ namespace Tests
             
             Console.WriteLine("Status: " + status);
             Console.WriteLine("Modelstatus: " + modelStatus);
+        
+            for (int i=0; i<sol.rowvalue.Length; i++) {
+                Console.WriteLine("Activity for row " + i + " = " + sol.rowvalue[i]);
+            }
+            for (int i=0; i<sol.coldual.Length; i++) {
+                Console.WriteLine("Reduced cost x[" + i + "] = " + sol.coldual[i]);
+            }
+            for (int i=0; i<sol.rowdual.Length; i++) {
+                Console.WriteLine("Dual value for row " + i + " = " + sol.rowdual[i]);
+            }
+            for (int i=0; i<sol.colvalue.Length; i++) {
+                Console.WriteLine("x" + i + " = " + sol.colvalue[i] + " is " + bas.colbasisstatus[i]);
+            }
+        }
+
+        [Test]
+        public void PassMip1Test()
+        {      
+            double[] cc = {1, 1}; // optim function koefs.
+            double[] cl = {2.1, 2.1}; // vars lower bounds
+            double[] cu = {8.5, 10.1}; // vars upper bounds
+            int[] varTypes = {1, 0}; // 0 - continuous, 1 - integer
+            int[] astart = {0, 2};
+            int[] aindex = {0, 1, 0};
+            double[] avalue = {1, 1, 1};
+            double[] rl = {5.2, 2};
+            double[] ru = {5.2, 4.5};
+            HighsObjectiveSense sense = HighsObjectiveSense.kMaximize;
+            double offset = 0;
+            HighsMatrixFormat a_format = HighsMatrixFormat.kColwise;
+
+            HighsModel model = new HighsModel(cc, cl, cu, rl, ru, astart, aindex, avalue, varTypes, offset, a_format, sense);
+
+            HighsLpSolver solver = new HighsLpSolver();
+
+            solver.passMip(model);
+            HighsStatus status = solver.run();
+            HighsSolution sol = solver.getSolution();
+            HighsBasis bas = solver.getBasis();
+            HighsModelStatus modelStatus = solver.GetModelStatus();
+            
+            Console.WriteLine("Status: " + status);
+            Console.WriteLine("Modelstatus: " + modelStatus);
+            Console.WriteLine("Optimization value: " + solver.getObjectiveValue());
         
             for (int i=0; i<sol.rowvalue.Length; i++) {
                 Console.WriteLine("Activity for row " + i + " = " + sol.rowvalue[i]);
